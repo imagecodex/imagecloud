6cda4be462038cde9840da0026e2243c76f3d0d8
songjiayang <songjiayang1@gmail.com>
2023-09-06 12:48:36 +0000

    image resize with pad color options
    Signed-off-by: songjiayang <songjiayang1@gmail.com>

--- internal/image/color/color_test.go
+++ internal/image/color/color_test.go
@@ -22,3 +22,21 @@ func TestHex2RGB(t *testing.T) {
 		assertion.Equal(color.B, uint8(0x2f))
 	}
 }
+
+func TestHex2RGBA(t *testing.T) {
+	assertion := assert.New(t)
+
+	{
+		_, err := Hex2RGBA("7ad42x00")
+		assertion.NotNil(err)
+	}
+
+	{
+		color, err := Hex2RGBA("7ad42f00")
+		assertion.Nil(err)
+		assertion.Equal(color.R, uint8(0x7a))
+		assertion.Equal(color.G, uint8(0xd4))
+		assertion.Equal(color.B, uint8(0x2f))
+		assertion.Equal(color.A, uint8(0x00))
+	}
+}
