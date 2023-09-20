e0ec4c3190f2cc7b932c5288d580511eeb86261d
songjiayang <songjiayang1@gmail.com>
2023-09-20 22:24:50 +0800

    bug fix for image auto rotate
    Signed-off-by: songjiayang <songjiayang1@gmail.com>

--- internal/image/processor/auto.go
+++ internal/image/processor/auto.go
@@ -15,21 +15,17 @@ func (*AutoRotate) Process(args *types.CmdArgs) (info *metadata.Info, err error)
 		return nil, errors.New("invalid auto orient params")
 	}
 
-	value, err := strconv.ParseInt(args.Params[0], 10, 64)
+	value, err := strconv.Atoi(args.Params[0])
 	if err != nil {
 		return nil, err
 	}
 
 	switch value {
-	case 0, 1:
+	case 0:
+		return nil, args.Img.RemoveOrientation()
+	case 1:
+		return nil, args.Img.AutoRotate()
 	default:
 		return nil, errors.New("auto orient value support (0, 1) only")
 	}
-
-	// ignore if value != 1
-	if value != 1 {
-		return
-	}
-
-	return nil, args.Img.AutoRotate()
 }
